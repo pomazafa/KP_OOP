692acc7b285b6b205ee80f8b24f924d7c8c2d522
pomazafa <p.duben@mail.ru>
2019-04-25 23:30:27 +0300

    changes in patient info

--- MyProject/MyProject/NewPatientWindow.xaml.cs
+++ MyProject/MyProject/NewPatientWindow.xaml.cs
@@ -24,11 +24,131 @@ namespace MyProject
             InitializeComponent();
         }
 
+        public NewPatientWindow(PATIENT p)
+        {
+            InitializeComponent();
+
+            Create.Content = "Изменить";
+
+            Surname.Text = p.SURNAME;
+            FirstName.Text = p.FIRSTNAME;
+            if(p.FATHERSNAME != null)
+            {
+                FatherName.Text = p.FATHERSNAME;
+            }
+            if(p.GENDER == "м")
+            {
+                GenderM.IsChecked = true;
+            }
+            if(p.GENDER == "ж")
+            {
+                GenderW.IsChecked = true;
+            }
+
+            if (p.BDAY != null)
+            {
+                DateBlock.Text = p.BDAY.Value.ToShortDateString();
+            }
+
+            if(p.TELEPHONE != null)
+            {
+                Telephone.Text = p.TELEPHONE;
+            }
+
+            MyDatabase db = new MyDatabase();
+
+            ADDRESS adr = db.ADDRESS.Find(p.ADDRESS_ID);
+
+            Street.Text = adr.STREET;
+
+            House.Text = adr.HOUSE;
+
+            if (adr.HOUSING != null)
+            {
+                Housing.Text = adr.HOUSING;
+            }
+
+            if (adr.FLAT != null)
+            {
+                Flat.Text = adr.FLAT.ToString();
+            }
+            p = db.PATIENT.Find(p.PATIENT_ID);
+            db.PATIENT.Remove(p);
+            db.SaveChanges();
+            db.ADDRESS.Remove(adr);
+            db.SaveChanges();
+        }
+
         private void Return_Click(object sender, RoutedEventArgs e)
         {
             SearchPatient wind = new SearchPatient();
             wind.Show();
             Close();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if(Surname.Text != "" && FirstName.Text != "" && Street.Text != "" && House.Text != "")
+            {
+                PATIENT newP = new PATIENT();
+                newP.SURNAME = Surname.Text;
+                newP.FIRSTNAME = FirstName.Text;
+                if (FatherName.Text != "")
+                    newP.FATHERSNAME = FatherName.Text;
+                if(GenderW.IsChecked == true)
+                {
+                    newP.GENDER = "ж";
+                }
+                else
+                {
+                    newP.GENDER = "м";
+                }
+
+                DateTime dt;
+                if (DateTime.TryParse(DateBlock.Text, out dt))
+                {
+                    newP.BDAY = dt;
+                }
+
+                if(Telephone.Text != "")
+                {
+                    newP.TELEPHONE = Telephone.Text;
+                }
+
+                ADDRESS newAdr = new ADDRESS();
+                newAdr.STREET = Street.Text;
+                newAdr.HOUSE = House.Text;
+
+                if(Housing.Text != "")
+                {
+                    newAdr.HOUSING = Housing.Text;
+                }
+                int flat;
+                if(Flat.Text != "" && int.TryParse(Flat.Text, out flat))
+                {
+                    newAdr.FLAT = flat;
+                }
+
+                MyDatabase db = new MyDatabase();
+                db.ADDRESS.Add(newAdr);
+
+                db.SaveChanges();
+
+                newP.ADDRESS_ID = newAdr.ADDRESS_ID;
+
+                db.PATIENT.Add(newP);
+
+                db.SaveChanges();
+
+                SearchPatient wind = new SearchPatient();
+                wind.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Заполните поля!!!!!!");
+            }
         }
     }
 }
--- MyProject/MyProject/SearchPatient.xaml.cs
+++ MyProject/MyProject/SearchPatient.xaml.cs
@@ -105,39 +105,17 @@ namespace MyProject
                     ResSet.Items.Refresh();
                 }
             }
-            //var r = context1.OrderedGoods;
-            //List<OrderedGoods> lst = new List<OrderedGoods>();
-            //if (dg2.SelectedItem != null)
-            //{
-            //    Orders order = dg2.SelectedItem as Orders;
-            //    foreach (OrderedGoods item in r)
-            //    {
-            //        if (item.OrderID == order.OrderID)
-            //        {
-            //            lst.Add(item);
-            //        }
-            //    }
-            //    AddProductOrder d = new AddProductOrder(order, lst);
-            //    if (d.ShowDialog() == true)
-            //    {
-            //        context1.SaveChanges();
-            //        var show1 = context1.OrderedGoods;
-            //        dg1.ItemsSource = null;
-            //        dg1.Items.Clear();
-            //        foreach (OrderedGoods m in show1)
-            //        {
-            //            if (m.OrderID == order.OrderID)
-            //            {
-            //                dg1.Items.Add(m);
-            //            }
-            //        }
-            //        dg1.Items.Refresh();
-
-            //    }
-            //}
-            //else MessageBox.Show("Выберите заказ для добавления товара(oв)", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
-
         }
 
+        private void ChangeInfo_Click(object sender, RoutedEventArgs e)
+        {
+            if (ResSet.SelectedItem != null)
+            {
+                PATIENT p = (PATIENT)ResSet.SelectedItem;
+                NewPatientWindow wind = new NewPatientWindow(p);
+                wind.Show();
+                Close();
+            }
+        }
     }
 }
